98b22fcaece2a7911e605b8d3cffd2a0d41244b5
admin <j.woodson@hotmail.fr>
2021-02-18 09:11:46 +0100

    controlleur livre debut

---
+++ TrocCommunity.WebUi.Tests/Controllers/LivreControllerTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TrocCommunity.WebUi.Tests.Controllers
+{
+    [TestClass]
+    public class LivreControllerTest
+    {
+
+        [TestInitialize]
+        public void Setup()
+        {
+
+        }
+            [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ TrocCommunity.WebUi/Controllers/BooksController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TrocCommunity.WebUi.Controllers
+{
+    public class BooksController : Controller
+    {
+        // GET: Books
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
